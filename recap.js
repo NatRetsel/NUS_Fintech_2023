b1307769bcea6181d6a461a7bbf17ac7e5583b9f
Lester Tan <127372303+SunsetStories@users.noreply.github.com>
2023-05-19 09:55:48 +0800

    Initiated node project

---
+++ recap.js
@@ -0,0 +1,28 @@
+let x = 7;
+let y = 5;
+let z = x + y;
+
+let age = "";
+
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+function subtraction(num1, num2) {
+    return num1 - num2;
+}
+
+function multiplication(num1, num2) {
+    return num1 * num2;
+}
+
+function isLeapYear(year) {
+    if (year % 400 == 0)
+        return true;
+    if (year % 100 == 0)
+        return false;
+    if (year % 4 == 0)
+        return true;
+    return false;
+    
+}
